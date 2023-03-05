8f2062b017e0d673d95116808b9bd11bb7411cd7
abid982 <abidarif982@gmail.com>
2023-03-05 08:53:10 +0500

    adding links and building spa

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,3 +2,8 @@
 module.exports = {
   extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
 };
+
+/**
+ *
+The file. eslintrc.js is a configuration file for a tool named ESLINT. ESLint is a tool for identifying and reporting on patterns found in ECMAScript/JavaScript code, with the goal of making code more consistent and avoiding bugs.22-No
+ */
--- app/routes/demo.jsx
+++ app/routes/demo.jsx
@@ -1,3 +1,3 @@
 export default function DemoPage() {
-  return <h1>Demo Page</h1>;
+    return <h1>Demo Page</h1>;
 }
