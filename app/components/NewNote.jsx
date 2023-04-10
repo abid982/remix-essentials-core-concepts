e892c1dd98899fc1757ee9818656967b3e979c33
abid982 <abidarif982@gmail.com>
2023-04-10 13:05:20 +0500

    fetch data from database by loader, return fetched data and use by use loader data hook, provide feedback to user by use navigation hook, validate input data and return data in action

--- app/components/NewNote.jsx
+++ app/components/NewNote.jsx
@@ -1,8 +1,31 @@
+import { Form, useActionData, useNavigation } from '@remix-run/react';
+
 import styles from './NewNote.css';
 
 function NewNote() {
+  const data = useActionData();
+
+  // This hook gives us navigation object
+  // Enhance the User Interface
+  const navigation = useNavigation();
+  // This navigation object contains some useful data about ongoing request that might be happening behind the scenes
+  // For example we get state whether we're submitting data
+  // navigation.state === 'idle';
+  // navigation.state === 'loading';
+  // navigation.state === 'submitting';
+  // We also get acces to submission to get more information about the latest form submission that was triggered like for example the path to which the request was sent, what the method was or which kind of data was submitted.
+  // navigation.submission.
+  const isSubmitting = navigation.state === 'submitting';
+  console.log('Is submitting form:');
+  console.log(isSubmitting);
+
   return (
-    <form method="post" id="note-form">
+    // It doesn't reload page and instead of triggering a new request that would actually fetch a new page
+    // Form Component provided by Remix
+
+    // *Note: The useActionData() and useLoaderData() cannot just be called this route components i.e /notes but in any component and they will get the action loader data of the closest loader or action that was called.
+    <Form method="post" id="note-form">
+      {data?.message && <p>{data.message}</p>}
       <p>
         <label htmlFor="title">Title</label>
         <input type="text" id="title" name="title" required />
@@ -12,9 +35,12 @@ function NewNote() {
         <textarea id="content" name="content" rows="5" required />
       </p>
       <div className="form-actions">
-        <button>Add Note</button>
+        {/* Disable button while the data is submitted */}
+        <button disabled={isSubmitting}>
+          {isSubmitting ? 'Adding...' : 'Add Note'}
+        </button>
       </div>
-    </form>
+    </Form>
   );
 }
 
--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,19 +1,52 @@
-import { redirect } from '@remix-run/node';
+import { json, redirect } from '@remix-run/node';
+import { useActionData, useLoaderData } from '@remix-run/react';
 import NewNote, { links as newNoteLinks } from '~/components/NewNote';
 // import newNotesStyles from '~/components/NewNote.css';
+
+import NoteList, { links as noteListLinks } from '~/components/NoteList';
 
 import { getStoredNotes, storeNotes } from '~/data/notes';
 
 export default function NotesPage() {
+  // In this component we now gets access to these notes to the data returned by the loader by using a special hook provided by Remix i.e the useLoaderData() hook
+  // The loader data that gets access to the data returned by loader
+  const notes = useLoaderData();
+
+  // The data is anything and in our case it's an object with the message property
+  // const data = useActionData();
+  // console.log('Data returned by an action:');
+  // console.log(data);
+
+  console.log('Notes data:');
+  console.log(notes);
   return (
     <main>
       {/* <h2>My Notes</h2> */}
+      {/* These components can get access to the loader and action data of this route i.e /notes */}
       <NewNote />
+      <NoteList notes={notes} />
     </main>
   );
 }
 
 // Backend code
+
+export async function loader() {
+  console.log('Loader!!!');
+  // return null;
+
+  const notes = await getStoredNotes();
+
+  // Serialized data
+  return notes;
+
+  // return new Response(JSON.stringify(notes), {
+  //   headers: { 'Content-Type': 'application/json' },
+  // });
+
+  // return json(notes);
+}
+
 export async function action({ request }) {
   // console.log('Data:');
   // console.log(data);
@@ -22,15 +55,25 @@ export async function action({ request }) {
   console.log(formData);
 
   // Extract value by name by get method
-  // const notesData = {
+  // const noteData = {
   //   title: formData.get('title'),
   //   content: formData.get('content'),
   // };
 
   // Shortcut
-  const notesData = Object.fromEntries(formData);
+  const noteData = Object.fromEntries(formData);
 
   // Add validation...
+  if (noteData.title.trim().length < 5) {
+    // Show error message to the user
+    // alert() You can't use browser APIs here
+    // Return a different value
+    // So actions like loaders can also return data not just redirect response
+    // Success message
+    // How do you get access that returned data?
+    // Well for that there is another hook which you typically use and that is useActionData() hook
+    return { message: 'Invalid title - must be atleast 5 characters long.' };
+  }
 
   // Add notes data as a new note to existing notes
 
@@ -38,19 +81,21 @@ export async function action({ request }) {
   const existingNotes = await getStoredNotes();
 
   // Unique identifier
-  notesData.id = new Date().toISOString();
+  noteData.id = new Date().toISOString();
 
-  const updateNotes = existingNotes.concat(notesData);
+  const updateNotes = existingNotes.concat(noteData);
 
   await storeNotes(updateNotes);
 
+  // await new Promise((resolve, reject) => setTimeout(() => resolve(), 2000));
+
   // Redirect the user after doing something on the server
   // redirect response
-  return redirect('/');
+  return redirect('/notes');
 }
 
 export function links() {
-  return [...newNoteLinks()];
+  return [...newNoteLinks(), ...noteListLinks()];
 }
 
 // export function links() {
